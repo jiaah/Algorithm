9516e6ec37d6fc34cdf86546aa85d72532d95c4a
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2024-06-02 17:21:33 +0900

    Time: 68 ms (24.97%), Space: 51.4 MB (84.14%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.ts
@@ -0,0 +1,11 @@
+function gcdOfStrings(str1: string, str2: string): string {
+   if(str1 + str2 !== str2 + str1) return '';
+
+   const gcd = (a, b) => {
+    return b === 0 ? a : gcd(b, a % b);
+   }
+
+   const gcdLength = gcd(str1.length, str2.length); 
+
+   return str1.substring(0, gcdLength);
+};
