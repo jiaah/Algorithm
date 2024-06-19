2e05ba04a01ce21a18790971e0df7e07de2c62cb
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2024-06-19 21:17:54 +0900

    Time: 59 ms (54.54%), Space: 50.4 MB (75.96%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.ts
+++ 0392-is-subsequence/0392-is-subsequence.ts
@@ -1,7 +1,4 @@
 function isSubsequence(s: string, t: string): boolean {
-    if(!s.length) return true;
-    if(!t.length) return false;
-
     const arr = s.split('');
 
     for(let j = 0; j < t.length; j++){
