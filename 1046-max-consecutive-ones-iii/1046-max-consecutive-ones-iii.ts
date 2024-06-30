1c76eb08e9ebcf302131b09afd99e909e7196855
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2024-06-30 17:16:03 +0900

    Time: 75 ms (31.01%), Space: 55.5 MB (45.29%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.ts
@@ -0,0 +1,20 @@
+function longestOnes(nums: number[], k: number): number {
+    let start = 0, end = 0, zeros = 0, maximum = 0;
+
+    while(end < nums.length){
+        
+        if(nums[end] === 0) {
+            zeros++;
+        }
+        end++;
+
+        if(zeros > k) { 
+            if(nums[start] === 0) {
+                zeros--;
+            }
+            start++;
+        }
+    }
+
+    return end - start;
+};
