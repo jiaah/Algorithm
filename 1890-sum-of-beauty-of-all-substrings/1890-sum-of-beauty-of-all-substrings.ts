af3f128d11edb9422ac4b9eae27e3cbd01dc2179
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2024-08-03 22:22:03 +0900

    Time: 322 ms (71.43%), Space: 56 MB (28.57%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.ts
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.ts
@@ -2,6 +2,18 @@ function beautySum(s: string): number {
     let totalBeauty = 0;
     const n = s.length;
 
+    function beauty(frequencyMap: number[]): number {
+        let maxFreq = 0, 
+            minFreq = n;
+
+        for(let num of frequencyMap){
+            if(!num) continue;
+            maxFreq = Math.max(maxFreq, num);
+            minFreq = Math.min(minFreq, num);
+        }
+        return maxFreq - minFreq;
+    }
+
     for(let i = 0; i < n; i++){ // starting point of a substring
         const freq = Array(26).fill(0); // frequency array for lowercase English letters
        
@@ -9,15 +21,7 @@ function beautySum(s: string): number {
             const charIdx = s.charCodeAt(j) - 'a'.charCodeAt(0); //  to map characters 'a' through 'z' to indices 0 through 25 in an array
             freq[charIdx]++
 
-            let maxFreq = 0, 
-                minFreq = n;
-            for(let num of freq){
-                if(!num) continue;
-                maxFreq = Math.max(maxFreq, num);
-                minFreq = Math.min(minFreq, num);
-            }
-
-            totalBeauty += maxFreq - minFreq;
+            totalBeauty += beauty(freq);
         }
     }
     
