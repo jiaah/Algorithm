98f7fbd655a33ef67e8609c3b70ead60dc01250d
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2024-09-25 17:21:35 +0900

    Time: 223 ms (18.18%), Space: 57.1 MB (90.91%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.ts
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.ts
@@ -1,16 +1,19 @@
 function countCompleteSubarrays(nums: number[]): number {
-    const size = new Set(nums).size;
+    const numsSize = nums.length;
+    const distintSize = new Set(nums).size;
     let start = 0, count = 0;
 
-    for(let start = 0; start < nums.length; start++){
+    if(numsSize === distintSize) return 1;
+
+    for(let start = 0; start < numsSize; start++){
         const sliced = new Set();
 
-        for(let end = start; end < nums.length; end++){
-        
+        for(let end = start; end < numsSize; end++){
             sliced.add(nums[end]);
         
-            if(sliced.size === size){
-                count++;
+            if(sliced.size === distintSize){
+                count += numsSize - end;
+                break;
             }
         }
     }
