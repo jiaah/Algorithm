eac284a26d2dc4e4288ccf6919b4bd26e27fe7fc
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2024-11-01 13:53:48 +0900

    Time: 13 ms (76.17%), Space: 53.3 MB (76.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,6 +1,7 @@
 function isAnagram(s: string, t: string): boolean {
         if(s.length !== t.length) return false;
 
+        // Hash Map Approach
         const set = {};
         for(let i = 0; i < s.length; i++) {
             // A single object set counts characters from s by adding and from t by subtracting.
@@ -14,3 +15,12 @@ function isAnagram(s: string, t: string): boolean {
         }
         return true;
 };
+
+// time complexity -> O(n)
+// space complexity -> O(1)
+
+// * Sorting Approach
+// It has a simpler code structure with a time complexity of O(nlogn). 
+// n = 5x10^4, logn = 16.6, nlogn = 8.3 * 10^5
+// Modern computers can perform millions of operations per second, 
+// so this number of operations can typically be completed within milliseconds.
