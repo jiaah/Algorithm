4659074da58a40aed348d5015e156ddf0ef3ec7c
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2024-11-12 23:59:10 +0900

    Time: 16 ms (59.06%), Space: 52.7 MB (72.34%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.ts
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.ts
@@ -1,29 +1,30 @@
 // Use the sliding window technique with start and end pointers.
+// The end pointer iterates over the string, expanding the window, and the start pointer adjusts to shrink the window when necessary.
 function characterReplacement(s: string, k: number): number {
     let longest = 0;
-    let start = 0; // The end pointer iterates over the string, expanding the window, and the start pointer adjusts to shrink the window when necessary
-    let group = {}; // Keeps track of the frequencies of characters in the current window
+    let start = 0; // start pointer of the window
+    let group = {}; // Character frequency in the window
+    let maxF = 0; // Max frequency of any character in the window
 
     for(let end = 0; end < s.length; end++) {
-        group[s[end]] = (group[s[end]] || 0) + 1; // Update character frequency
-        const counts = Object.values(group) as number[]; // Get all frequencies
+        group[s[end]] = (group[s[end]] || 0) + 1; // Update frequency
+        maxF = Math.max(maxF, group[s[end]]); // Track max frequency
 
-        // Window Validity Check
-        // If "(window size) - (the max frequency) > k", shrink the window
-        if((end - start + 1) - Math.max(...counts) > k) {  
-            group[s[start]] = group[s[start]] - 1; // Decrease frequency of the leftmost character
-            start++; // Move the start pointer to the right
+        // Shrink window if condition is violated
+        if((end - start + 1) - maxF > k) {  
+            group[s[start]] = group[s[start]] - 1; // Remove leftmost character
+            start++; // Move start pointer to the right
             continue;
         }
 
-        // else, considered valid
-        longest = Math.max(longest, end - start + 1); // Update the longest length whenever the window is valid
+        // Update longest valid window
+        longest = Math.max(longest, end - start + 1);
     }
     return longest;
 };
       
-// TC: O(n * m) -  n: string length, m:number of unique characters in the window
-// SC: O(m) - number of unique characters in the window
+// TC: O(n) - end pointer iterates through the string once
+// SC: O(m) - stores frequencies of unique characters in the window
 
        
         
