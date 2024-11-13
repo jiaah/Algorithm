eb12088f73800d26e16569c239459fb138533ad8
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2024-11-13 18:09:37 +0900

    Time: 33 ms (50%), Space: 57.3 MB (41.89%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.ts
+++ 0567-permutation-in-string/0567-permutation-in-string.ts
@@ -1,11 +1,12 @@
 function checkInclusion(s1: string, s2: string): boolean {
+    if(s1.length > s2.length) return false;
+
     // s1의 문자 빈도수 계산
     const s1Group = {};
     for(let char of s1) {
         s1Group[char] = (s1Group[char] || 0) + 1;
     }
 
-    let result = false;
     let start = 0
     const count = s1.length;
     const s2Group = {}
@@ -16,7 +17,7 @@ function checkInclusion(s1: string, s2: string): boolean {
         if(end - start + 1 !== count) continue; 
 
         // 윈도우가 s1의 길이와 같아지면 유효성 검증
-        if(Object.keys(s1Group).every(key => s1Group[key] === s2Group[key])){
+        if(isMatching(s1Group, s2Group)){
             return true;
         }
 
@@ -25,8 +26,16 @@ function checkInclusion(s1: string, s2: string): boolean {
         if(!s2Group[s2[start]]) { delete s2Group[s2[start]]; }; // 필요하지 않은 키는 삭제하여 메모리 절약
         start++;
     }   
-    return result 
+    return false; 
 };
+
+interface Group {
+    [key: string]: number;
+}
+
+function isMatching(s1Group:Group, s2Group:Group):boolean {
+    return Object.keys(s1Group).every(key => s1Group[key] === s2Group[key]);
+}
 
 // TC: O(m * n) - s2(n)를 순회하면서 s1(m)과 같은 길이의 윈도우에서 빈도수를 검사
 // SC: O(m) - s1Group과 s2Group이 각각 O(m)의 공간을 사용
