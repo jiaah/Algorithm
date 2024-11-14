1fe6f7d4f555276b176898ee5e154449e1c7c305
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2024-11-14 12:35:48 +0900

    Time: 160 ms (29.27%), Space: 60 MB (6.78%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.ts
+++ 0567-permutation-in-string/0567-permutation-in-string.ts
@@ -1,48 +1,52 @@
 function checkInclusion(s1: string, s2: string): boolean {
-    if(s1.length > s2.length) return false;
-
-    // s1의 문자 빈도수 계산
-    const s1Group = {};
-    for(let char of s1) {
-        s1Group[char] = (s1Group[char] || 0) + 1;
+    if (s1.length > s2.length) {
+        return false;
     }
 
-    let start = 0
-    const count = s1.length;
-    const s2Group = {}
+    let s1Count = new Array(26).fill(0);
+    let s2Count = new Array(26).fill(0);
+    for (let i = 0; i < s1.length; i++) {
+        s1Count[s1.charCodeAt(i) - 97]++;
+        // 처음에 s1.length(window 크기)만큼만 카운팅하고, 그 후에 슬라이딩 윈도우를 사용하여 윈도우를 하나씩 이동시키면서 다른 부분 문자열들을 검사
+        s2Count[s2.charCodeAt(i) - 97]++; 
+    }
 
-    // s2에서 슬라이딩 윈도우로 탐색
-    for(let end = 0; end < s2.length; end++) {
-        s2Group[s2[end]] = (s2Group[s2[end]] || 0) + 1;
-        if(end - start + 1 !== count) continue; 
+    let matches = 0; // s2 윈도우에서 현재 상태가 s1의 상태와 얼마나 일치하는지를 알려주는 지표 역할
+    for (let i = 0; i < 26; i++) {
+        if (s1Count[i] === s2Count[i]) {
+            matches++;
+        }
+    }
 
-        // 윈도우가 s1의 길이와 같아지면 유효성 검증
-        if(isMatching(s1Group, s2Group)){
+    let l = 0;
+    for (let r = s1.length; r < s2.length; r++) {
+        if (matches === 26) { // 모든 알파벳의 빈도가 일치하는 상태
             return true;
         }
 
-        // 유효하지 않은 window인 경우, 윈도우의 시작 문자를 제거
-        s2Group[s2[start]] -= 1;
-        if(!s2Group[s2[start]]) { delete s2Group[s2[start]]; }; // 필요하지 않은 키는 삭제하여 메모리 절약
-        start++;
-    }   
-    return false; 
+        // 윈도우를 하나씩 오른쪽으로 이동시키면서 s1Count와 s2Count를 비교
+        let index = s2.charCodeAt(r) - 97;
+        s2Count[index]++;
+        console.log('#',r, s1Count[index], s2Count[index] )
+        if (s1Count[index] === s2Count[index]) { // 현재 일치하는 경우
+            matches++;
+        } else if (s1Count[index] + 1 === s2Count[index]) { // 이전에 일치 상태가 깨진 경우
+            matches--;
+        } else { } // 이전에도 일치하지 않았고, 현재도 일치하지 않는 경우
+                   // e.g. s1="ab", s2="eidbzaz", r=6 -> s1Count[z]=0, s2Count[z]=2
+            
+        // 왼쪽 끝의 문자(s2[l])를 윈도우 밖으로 내보내면서 s1Count와 s2Count를 비교
+        index = s2.charCodeAt(l) - 97;
+        s2Count[index]--;        
+        if (s1Count[index] === s2Count[index]) { // 현재 일치하는 경우
+            matches++;
+        } else if (s1Count[index] - 1 === s2Count[index]) { // 이전에 일치 상태가 깨진 경우
+            matches--;
+        } else { } // 이전에도 일치하지 않았고, 현재도 일치하지 않는 경우
+        l++;
+    }
+    return matches === 26;
 };
 
-interface Group {
-    [key: string]: number;
-}
-
-// s1Group과 s2Group의 빈도수가 일치하는지 확인하는 함수
-function isMatching(s1Group:Group, s2Group:Group):boolean {
-    // 각 문자의 차이만 추적하여 O(1)로 최적화
-    for(let key in s1Group) {
-        if(s1Group[key] !== s2Group[key]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-// TC: O(n) - s2(n)를 순회
-// SC: O(m) - s1의 길이에 따라 s1Group과 s2Group이 사용하는 공간
+// TC: O(n)
+// SC: O(1)
