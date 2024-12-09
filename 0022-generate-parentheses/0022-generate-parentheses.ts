7db95782b6dbd7005fd56734d6d901078ee6e1e4
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2024-12-09 15:14:39 +0900

    Time: 2 ms (73.65%), Space: 51.8 MB (58.45%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.ts
@@ -0,0 +1,20 @@
+function generateParenthesis(n: number): string[] {
+    const res:string[] = [];
+    
+    const backtrack = (openN, closeN, stack) => {
+        if(openN === closeN && openN === n) {
+            res.push(stack);
+            return;
+        };
+
+        if(openN < n) backtrack(openN + 1, closeN, stack + '(');
+        if(closeN < openN) backtrack(openN, closeN + 1, stack + ')');
+    };
+
+    backtrack(0, 0, '');
+    return res;
+};
+
+// Cn = n^4/sqrt(n), The number of valid parenthesis combinations (Catalan number)
+// TC: O(Cn), Iterates for each valid parenthesis combination
+// SC: O(n * Cn), Includes both result storage and recursion stack
