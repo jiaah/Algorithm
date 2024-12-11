76e455a2f563cd1dd0c9d25a87208bde5c658990
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2024-12-11 12:21:36 +0900

    Time: 176 ms (22.93%), Space: 77.2 MB (53.03%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.ts
+++ 0883-car-fleet/0883-car-fleet.ts
@@ -1,25 +1,24 @@
 type Car = [time: number, speed: number];
 
 function carFleet(target: number, position: number[], speed: number[]): number {
-    const fleetTimes:number[] = [];
     const cars:Car[] = position
         .map((x, index):Car => [x, speed[index]])
         .sort((a, b) => b[0] - a[0]);
 
+    let fleets = 0;
+    let reachingTime = 0;
+
     for(const [x, s] of cars){
         const time = (target - x)/s;
-        const lastFleetTime = fleetTimes[fleetTimes.length - 1];
 
-        if(lastFleetTime && lastFleetTime > time) {
-            fleetTimes.push(lastFleetTime); // Merge into the current fleet
-        } else {
-            fleetTimes.push(time); // Start a new fleet
-        }
+        if(time > reachingTime) {
+            fleets++; // Start a new fleet
+            reachingTime = time; 
+        } 
     }
 
-    const uniquefleets = new Set(fleetTimes);
-    return uniquefleets.size;
+    return fleets;
 };
 
 // TC: O(n)
-// SC: O(n)
+// SC: O(1)
