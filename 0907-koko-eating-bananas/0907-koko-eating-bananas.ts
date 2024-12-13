7e337f9c90c29822e7690adfb3c035a6a0af65f1
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2024-12-13 14:35:15 +0900

    Time: 11 ms (50.55%), Space: 53.4 MB (63.01%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.ts
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.ts
@@ -2,18 +2,24 @@ function minEatingSpeed(piles: number[], h: number): number {
     let left = 1;
     let right = Math.max(...piles);
     let minSpeed = right;
+
+    const canEatAll = (speed) => {
+        let totalTime = 0;
+
+        for(let pile of piles){
+            totalTime += Math.ceil(pile / speed); 
+            if(totalTime > h) { return false };
+        }
+        return true;
+    }
     
     while(left <= right) {
         const speed = Math.floor((left + right) / 2);
-        let totalTime = 0;
 
-        piles.forEach(pile => {
-            totalTime += Math.ceil(pile / speed); 
-        }) 
 
-        if(totalTime <= h){
-            minSpeed = speed; // Update minSpeed to the current valid candidate
-            right = speed - 1;  // Narrow the range to search for smaller valid speeds
+        if(canEatAll(speed)){
+            minSpeed = speed; // Try for a smaller valid speed
+            right = speed - 1;  // Increase speed to meet time constraint
         } else {
             left = speed + 1;
         }
