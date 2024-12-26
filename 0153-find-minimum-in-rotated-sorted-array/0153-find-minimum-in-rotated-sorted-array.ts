72205484b28859439f6eb46ea427c826b815bdbf
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2024-12-26 11:40:39 +0900

    Time: 0 ms (100%), Space: 51.9 MB (7.59%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.ts
@@ -0,0 +1,18 @@
+function findMin(nums: number[]): number {
+    let left = 0; // start of the search lange
+    let right = nums.length - 1; // end of the search range
+
+    while(left < right){
+        const mid = Math.floor((left + right)/2);
+
+        if(nums[mid] > nums[right]) {
+            left = mid + 1; // minimum is in the right half
+        } else {
+            right = mid; // minimum is in the left haft or at mid
+        }
+    }
+    return nums[left]; // left pointer will the minumum
+};
+
+// TC: O(log n)
+// SC: O(1)
