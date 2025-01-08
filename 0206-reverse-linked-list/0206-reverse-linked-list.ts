679fed7da37e41874e1b4bc712aa8bfb59314d67
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2025-01-08 17:19:18 +0900

    Time: 2 ms (7.9%), Space: 52.6 MB (69.12%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let prev = null;
+    let current = head;
+
+    while(current) {
+        const next = current.next; // 다음 노드 저장
+
+        current.next = prev;; // 현재 노드의 next를 이전 노드로 변경
+ 
+        prev = current; // 이전 노드를 현재 노드로 업데이트
+        current = next; // 현재 노드를 다음 노드로 이동
+    };
+    return prev; // 새로운 헤드 반환
+};
+
+// TC: O(n)
+// SC: O(1)
