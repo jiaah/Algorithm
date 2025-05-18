0141fce127151b87f7addb16c403fda56df114e7
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2025-05-18 17:57:50 +0900

    Time: 3 ms (94.22%), Space: 60.1 MB (61.07%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -1,21 +1,19 @@
-// Sliding window technique
 function lengthOfLongestSubstring(s: string): number {
-    const set = new Set();
-    let start = 0, longest = 0;
-  
-    // 'end' extends the window.
-    for(let end = 0; end < s.length; end++){
-        // Move 'start' pointer to the right until the duplicate is removed
-        while(set.has(s[end])) {  // TC: O(1) per operation - 'start' pointer moves forward at most once per character
-            set.delete(s[start]);
+    const store = new Set(); 
+    let start = 0, end = 0, longest = 0;
+
+    while(end < s.length) {
+        if(!store.has(s[end])) {
+            store.add(s[end]);
+            end++;
+        } else {
+            store.delete(s[start]);
             start++;
         }
-
-        set.add(s[end]);
-        longest = Math.max(longest, set.size);
-    };
+        longest = Math.max(longest, store.size);
+    }
     return longest;
 };
 
 // TC: O(n)
-// SC: O(m) - total number of unique characters in the string
+// SC: O(n)
