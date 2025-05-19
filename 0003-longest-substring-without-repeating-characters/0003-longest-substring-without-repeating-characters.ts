1e1005a1160308bfee7c8499e2adf249c6a46fdf
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2025-05-19 12:43:54 +0900

    Time: 3 ms (94.28%), Space: 56.5 MB (99.48%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -3,12 +3,12 @@ function lengthOfLongestSubstring(s: string): number {
     let l = 0, longest = 0;
 
     for(let r = 0; r < s.length; r++) {
-        
         if(store.has(s[r])) {
             // 이전 중복 문자가 현재 윈도우 내에 있는 경우만 처리
             // -> 왼쪽 포인터를 "더 오른쪽으로"만 이동
             l = Math.max(l, store.get(s[r]) + 1); 
         }
+        
         longest = Math.max(longest, r - l + 1);
         store.set(s[r], r);
     }
