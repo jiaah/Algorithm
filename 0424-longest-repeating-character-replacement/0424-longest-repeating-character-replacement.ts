cda2461c40840fc792f8182e8c1ef2aa6436557c
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2025-05-22 12:36:37 +0900

    Time: 16 ms (79.22%), Space: 55.5 MB (99.03%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.ts
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.ts
@@ -1,30 +1,25 @@
-// Use the sliding window technique with start and end pointers.
-// The end pointer iterates over the string, expanding the window, and the start pointer adjusts to shrink the window when necessary.
 function characterReplacement(s: string, k: number): number {
-    let longest = 0;
-    let start = 0; // start pointer of the window
-    let group = {}; // Character frequency in the window
-    let maxF = 0; // Max frequency of any character in the window
+   let count = new Map<string, number>();
+   let l = 0, maxLen = 0, maxFreq = 0;
 
-    for(let end = 0; end < s.length; end++) {
-        group[s[end]] = (group[s[end]] || 0) + 1; // Update frequency
-        maxF = Math.max(maxF, group[s[end]]); // Track max frequency
+   for (let r = 0; r < s.length; r++) {    
+        const curCount = (count.get(s[r]) ?? 0) + 1;
+        count.set(s[r], curCount);
+        maxFreq = Math.max(maxFreq, curCount);
 
-        // Shrink window if condition is violated
-        if((end - start + 1) - maxF > k) {  
-            group[s[start]] = group[s[start]] - 1; // Remove leftmost character
-            start++; // Move start pointer to the right
-            continue;
-        }
-
-        // Update longest valid window
-        longest = Math.max(longest, end - start + 1);
-    }
-    return longest;
+        while((r - l + 1) - maxFreq > k) {
+            // while(windowSize - maxFreq > k) 
+            // 현재 윈도우에서 k개를 바꿔도 동일 문자로 채울 수 없는 경우 → 윈도우 축소
+            count.set(s[l], count.get(s[l]) - 1);
+            l++
+        }    
+        maxLen = Math.max(maxLen, r - l + 1);
+   }
+    return maxLen;
 };
-      
-// TC: O(n) - end pointer iterates through the string once
-// SC: O(m) - stores frequencies of unique characters in the window
+
+// TC: O(n)
+// SC: O(1)
 
        
         
