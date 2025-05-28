37f3cff4513ef800ad322b6066bbc237d82c82b0
Jiah Lee <25231717+jiaah@users.noreply.github.com>
2025-05-28 16:23:13 +0900

    Time: 3 ms (53.7%), Space: 66.4 MB (20.96%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,11 +1,9 @@
 function maxProfit(prices: number[]): number {
-    let l = 0, maxP = 0;
-    for(let r = 0; r < prices.length; r++) {
-        if(prices[l] < prices[r]) {
-            maxP = Math.max(maxP, prices[r] - prices[l]);
-        } else {
-            l = r;
-        }
+    let minBuy = prices[0], maxP = 0;
+    
+    for(let price of prices) {
+        maxP = Math.max(maxP, price - minBuy); // 현재 가격 - 최소 매수가 
+        minBuy = Math.min(minBuy, price); // 최소 매수가 갱신
     }
     return maxP;
 };
